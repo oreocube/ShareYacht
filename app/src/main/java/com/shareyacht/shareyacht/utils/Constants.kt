b34516fb3f55278445b89051680c6ec5442cd6de
oreocube <oreocube13@gmail.com>
2021-07-23 22:49:47 +0900

    Constants - 상수 모음

---
+++ app/src/main/java/com/shareyacht/shareyacht/utils/Constants.kt
@@ -0,0 +1,22 @@
+package com.shareyacht.shareyacht.utils
+
+object Constants {
+    const val TAG: String = "로그"
+}
+
+object API {
+    const val BASE_URL:String ="http://wsx1341.tplinkdns.com:8080"
+
+}
+
+object Keyword {
+
+}
+
+object Preference {
+    // Preference 이름
+    const val SHARED_PREFERENCE_FILE = "yacht_preference"
+
+    // Preference Key 값
+    const val SHARED_PREFERENCE_NAME_COOKIE = "cookies"
+}
