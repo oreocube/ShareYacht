04cc5f3e62dad9e8960459842d6ecfe9bc61a3bc
oreocube <oreocube13@gmail.com>
2021-07-23 20:47:57 +0900

    로그인, 회원가입 - 네비게이션 그래프로 구현

---
+++ app/src/main/java/com/shareyacht/shareyacht/UserInfoFragment.kt
@@ -0,0 +1,35 @@
+package com.shareyacht.shareyacht
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.shareyacht.shareyacht.databinding.FragmentUserInfoBinding
+
+class UserInfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentUserInfoBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_user_info, container, false)
+        context ?: return binding.root
+
+        binding.apply {
+
+            toolbar.apply {
+                title = getString(R.string.title_user_info)
+                setNavigationIcon(R.drawable.ic_back)
+                setNavigationOnClickListener { view ->
+                    view.findNavController().navigateUp()
+                }
+            }
+        }
+
+        return binding.root
+    }
+}
