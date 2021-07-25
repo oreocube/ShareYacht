931dd19a9e38a61ebc05b4d353d9033207618cbc
oreocube <oreocube13@gmail.com>
2021-07-25 18:09:21 +0900

    회원가입, 로그인 모델 추가

---
+++ app/src/main/java/com/shareyacht/shareyacht/model/User.kt
@@ -0,0 +1,25 @@
+package com.shareyacht.shareyacht.model
+
+import com.google.gson.annotations.SerializedName
+import com.shareyacht.shareyacht.utils.Keyword
+
+data class User(
+    @SerializedName(Keyword.ID) val email: String,
+    @SerializedName(Keyword.PASSWORD) val password: String,
+    @SerializedName(Keyword.USER_TYPE) val userType: Int,
+    @SerializedName(Keyword.NAME) val name: String,
+    @SerializedName(Keyword.ADDRESS) val address: String,
+    @SerializedName(Keyword.PHONE) val phone: String,
+    @SerializedName(Keyword.SEX) val sex: String
+)
+
+data class ReqLogin(
+    @SerializedName(Keyword.ID)
+    val email: String,
+
+    @SerializedName(Keyword.PASSWORD)
+    val password: String,
+
+    @SerializedName(Keyword.USER_TYPE)
+    val userType: Int
+)
