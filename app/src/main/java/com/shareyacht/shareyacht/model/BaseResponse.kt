9e34e676d4f85354aa225dc5fe81000bb825a685
oreocube <oreocube13@gmail.com>
2021-07-26 14:51:39 +0900

    BaseResponse : generic 으로 구현

--- app/src/main/java/com/shareyacht/shareyacht/model/BaseResponse.kt
+++ app/src/main/java/com/shareyacht/shareyacht/model/BaseResponse.kt
@@ -3,19 +3,13 @@ package com.shareyacht.shareyacht.model
 import com.google.gson.annotations.SerializedName
 import com.shareyacht.shareyacht.utils.Keyword
 
-abstract class Base{
-    abstract val error: Boolean
-    abstract val message: String
-    abstract val status: String
-}
-
-data class BaseResponse(
+data class BaseResponse<T>(
     @SerializedName(Keyword.ERROR)
-    override val error: Boolean,
-
+    val error: Boolean,
     @SerializedName(Keyword.MESSAGE)
-    override val message: String,
-
+    val message: String,
     @SerializedName(Keyword.STATUS)
-    override val status: String
-) : Base()
+    val status: String,
+    @SerializedName("data")
+    val data: T
+)
