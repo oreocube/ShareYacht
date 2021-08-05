eb061b5f6cb59dc9d3b64da8aee35531ccfcf2a0
oreocube <oreocube13@gmail.com>
2021-08-05 17:55:26 +0900

    요트 등록 : viewModel 연결

--- app/src/main/java/com/shareyacht/shareyacht/view/AddYachtActivity.kt
+++ app/src/main/java/com/shareyacht/shareyacht/view/AddYachtActivity.kt
@@ -2,16 +2,26 @@ package com.shareyacht.shareyacht.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.shareyacht.shareyacht.R
 import com.shareyacht.shareyacht.databinding.ActivityAddYachtBinding
+import com.shareyacht.shareyacht.viewmodel.AddYachtViewModel
 
 class AddYachtActivity : AppCompatActivity() {
+    private val viewModel: AddYachtViewModel by viewModels()
 
     private lateinit var mBinding: ActivityAddYachtBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mBinding = ActivityAddYachtBinding.inflate(layoutInflater)
-        setContentView(mBinding.root)
+        val binding: ActivityAddYachtBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_add_yacht
+        )
+
+        // viewModel 바인딩
+        binding.viewModel = viewModel
+
     }
 }
