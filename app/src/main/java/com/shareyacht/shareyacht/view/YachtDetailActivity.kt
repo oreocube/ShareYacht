91912c03a79cc515a09b330a7efcecd4de31fff2
oreocube <oreocube13@gmail.com>
2021-08-15 14:20:05 +0900

    요트 상세보기에서 버튼 클릭 시 예약신청 화면으로 전환

--- app/src/main/java/com/shareyacht/shareyacht/view/YachtDetailActivity.kt
+++ app/src/main/java/com/shareyacht/shareyacht/view/YachtDetailActivity.kt
@@ -1,7 +1,9 @@
 package com.shareyacht.shareyacht.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
@@ -12,14 +14,15 @@ import com.shareyacht.shareyacht.viewmodel.YachtDetailViewModel
 
 class YachtDetailActivity : AppCompatActivity() {
     private val viewModel: YachtDetailViewModel by viewModels()
+    private var yachtID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val yachtID = intent.getIntExtra(Keyword.YACHT_ID, 0)
 
         val binding = DataBindingUtil.setContentView<ActivityYachtDetailBinding>(
             this, R.layout.activity_yacht_detail
         )
+        yachtID = intent.getIntExtra(Keyword.YACHT_ID, 0)
 
         // 요트 정보 요청하기
         viewModel.getYachtDetail(yachtID)
@@ -34,5 +37,11 @@ class YachtDetailActivity : AppCompatActivity() {
                 .centerCrop()
                 .into(binding.yachtImage)
         })
+    }
+
+    fun reserveButtonClick(view:View) {
+        val intent = Intent(this, YachtReserveActivity::class.java)
+        intent.putExtra(Keyword.YACHT_ID, yachtID)
+        startActivity(intent)
     }
 }
