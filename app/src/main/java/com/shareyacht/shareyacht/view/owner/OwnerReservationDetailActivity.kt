59845e6739f0401dead10057c7e4f12e8175fd18
oreocube <oreocube13@gmail.com>
2021-08-19 18:41:21 +0900

    [사업자] 예약 상세 액티비티, 뷰모델 추가

---
+++ app/src/main/java/com/shareyacht/shareyacht/view/owner/OwnerReservationDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.shareyacht.shareyacht.view.owner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shareyacht.shareyacht.R
+
+class OwnerReservationDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owner_reservation_detail)
+    }
+}
---
+++ app/src/main/java/com/shareyacht/shareyacht/viewmodel/OwnerReserveDetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.shareyacht.shareyacht.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class OwnerReserveDetailViewModel : ViewModel() {
+}
