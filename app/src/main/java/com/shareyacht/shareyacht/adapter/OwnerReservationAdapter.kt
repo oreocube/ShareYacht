2330d280c51ac1f9454c7247abf76b5155ebb90d
oreocube <oreocube13@gmail.com>
2021-08-19 17:40:49 +0900

    [사업자] 예약현황 리사이클러뷰 어댑터 구현 완료

--- app/src/main/java/com/shareyacht/shareyacht/adapter/OwnerReservationAdapter.kt
+++ app/src/main/java/com/shareyacht/shareyacht/adapter/OwnerReservationAdapter.kt
@@ -1,21 +1,64 @@
 package com.shareyacht.shareyacht.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.shareyacht.shareyacht.databinding.RvItemOwnerReservationBinding
 import com.shareyacht.shareyacht.model.OwnerYachtReservation
+import com.shareyacht.shareyacht.utils.API
+import com.shareyacht.shareyacht.utils.formatter
 
 /* [사업자] 예약현황 - 상태별 목록 - list adapter */
 class OwnerReservationAdapter :
-    ListAdapter<OwnerYachtReservation, RecyclerView.ViewHolder>(RESERVATION_COMPARATOR) {
+    ListAdapter<OwnerYachtReservation, OwnerReservationAdapter.OwnerReservationViewHolder>(
+        RESERVATION_COMPARATOR
+    ) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OwnerReservationViewHolder {
+        val binding = RvItemOwnerReservationBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return OwnerReservationViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: OwnerReservationViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    class OwnerReservationViewHolder(private val binding: RvItemOwnerReservationBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(reservation: OwnerYachtReservation) {
+            val url = "${API.BASE_URL}/image/${reservation.yacht.imageid}"
+
+            // 출항 시각, 입항 시각
+            val start = reservation.departure.substring(15, 17).toInt()
+            val end = reservation.arrival.substring(15, 17).toInt()
+            val time = end - start
+            // 금액 합계
+            val totalPrice = reservation.yacht.price.toInt() * reservation.embarkCount * time
+
+            binding.apply {
+                yachtReservation = reservation
+                price = formatter.format(totalPrice)
+
+                Glide.with(itemView)
+                    .load(url)
+                    .centerCrop()
+                    .into(yachtImage)
+
+                itemView.setOnClickListener {
+                    // 나중에
+                }
+            }
+        }
     }
 
     // COMPARATOR
