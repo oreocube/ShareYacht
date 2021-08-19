1faefc8746dfaca889d04496f0f066849eb81308
oreocube <oreocube13@gmail.com>
2021-08-19 17:41:17 +0900

    [사업자] 예약현황 프래그먼트 : status 에 맞는 리스트를 어댑터에 submit

--- app/src/main/java/com/shareyacht/shareyacht/view/owner/ReservationFragment.kt
+++ app/src/main/java/com/shareyacht/shareyacht/view/owner/ReservationFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.shareyacht.shareyacht.adapter.OwnerReservationAdapter
 import com.shareyacht.shareyacht.databinding.FragmentReservationStateBinding
+import com.shareyacht.shareyacht.utils.Constants.STATE_CANCEL
+import com.shareyacht.shareyacht.utils.Constants.STATE_CONFIRMED
+import com.shareyacht.shareyacht.utils.Constants.STATE_MOVING
+import com.shareyacht.shareyacht.utils.Constants.STATE_WAIT
+import com.shareyacht.shareyacht.viewmodel.OwnerReserveViewModel
 
 /* [사업자] 예약현황 - 상태별 목록을 보여줄 Fragment (Pager adapter 에서 사용) */
 class ReservationFragment(val state: Int) : Fragment() {
     private var mBinding: FragmentReservationStateBinding? = null
+    private val viewModel: OwnerReserveViewModel by activityViewModels()
     private lateinit var mAdapter: OwnerReservationAdapter
 
     override fun onCreateView(
@@ -23,6 +31,30 @@ class ReservationFragment(val state: Int) : Fragment() {
         mBinding = binding
 
         mAdapter = OwnerReservationAdapter()
+
+        viewModel.filterCompleted.observe(viewLifecycleOwner, {
+            if (it) {
+                when (state) {
+                    STATE_WAIT -> {
+                        mAdapter.submitList(viewModel.stateWaitList)
+                    }
+                    STATE_CONFIRMED -> {
+                        mAdapter.submitList(viewModel.stateConfirmedList)
+                    }
+                    STATE_MOVING -> {
+                        mAdapter.submitList(viewModel.stateMovingList)
+                    }
+                    STATE_CANCEL -> {
+                        mAdapter.submitList(viewModel.stateCancelList)
+                    }
+                }
+            }
+        })
+
+        binding.recyclerview.apply {
+            this.adapter = mAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
 
         return binding.root
     }
