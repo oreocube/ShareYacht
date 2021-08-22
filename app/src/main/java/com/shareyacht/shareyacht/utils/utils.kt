58669e9a70a9d117be7169682e500803894e21a9
oreocube <oreocube13@gmail.com>
2021-08-23 04:16:43 +0900

    getNowTime() : 현재 시각을 구함

--- app/src/main/java/com/shareyacht/shareyacht/utils/utils.kt
+++ app/src/main/java/com/shareyacht/shareyacht/utils/utils.kt
@@ -1,5 +1,13 @@
 package com.shareyacht.shareyacht.utils
 
 import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 val formatter = DecimalFormat("#,###")
+
+fun getNowTime(): String {
+    val currentTime = Calendar.getInstance().time
+    val sdf = SimpleDateFormat("yyyy-MM-dd E요일", Locale.KOREAN)
+    return sdf.format(currentTime)
+}
